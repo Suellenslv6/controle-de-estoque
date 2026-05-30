fdc6cbbe91bd758d49305f83a58c3d97ce8dd1ff
Suellenslv6 <132712783+Suellenslv6@users.noreply.github.com>
2026-05-30 13:35:43 -0300

    Atualizar o estoque.java

--- estoque.java
+++ estoque.java
@@ -1,15 +1,5 @@
 import java.util.Scanner;
 
-public class Produto {
-
-    String nome;
-    int qtdEstoque;
-    double precoUnitario;
-    String categoria;
-    int qtdMinima;
-
-}
-
 public class ControleEstoque {
 
     static Scanner sc = new Scanner(System.in);
